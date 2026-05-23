565d518a676389e2e32ea692ec650546bfbbd7b2
unknown <rohinikumar40968@gmail.com>
2026-05-23 15:50:22 +0530

    23/05/2026 pg deploy

--- wwwroot/js/ChartScripts/DateTimeUIScripts/z.datetime.init.js
+++ wwwroot/js/ChartScripts/DateTimeUIScripts/z.datetime.init.js
@@ -1,6 +1,18 @@
 ﻿// Set Localization...
 $.datetimepicker.setLocale('en');
 
+// Helper: format a Date as yyyy/MM/dd HH:mm (matches picker format Y/m/d H:i)
+function gvDtNowFormatted() {
+    var d = new Date();
+    var pad = function(n) { return n < 10 ? '0' + n : n; };
+    return d.getFullYear() + '/' + pad(d.getMonth() + 1) + '/' + pad(d.getDate()) + ' ' + pad(d.getHours()) + ':' + pad(d.getMinutes());
+}
+function gvDateOnlyFormatted() {
+    var d = new Date();
+    var pad = function(n) { return n < 10 ? '0' + n : n; };
+    return d.getFullYear() + '/' + pad(d.getMonth() + 1) + '/' + pad(d.getDate());
+}
+
 // Default 7 days range.
 var beginDate = new Date();
 beginDate.setDate(beginDate.getDate() - 7);
@@ -10,7 +22,9 @@ $('#BeginDate').datetimepicker({
     dayOfWeekStart: 1,
     lang: 'en',
     value: beginDate,
-    step: 5
+    step: 5,
+    maxDate: gvDateOnlyFormatted(),
+    maxTime: gvDtNowFormatted()
 });
 
 $('#EndDate').datetimepicker({
@@ -18,7 +32,9 @@ $('#EndDate').datetimepicker({
     dayOfWeekStart: 1,
     lang: 'en',
     value: new Date(),
-    step: 5
+    step: 5,
+    maxDate: gvDateOnlyFormatted(),
+    maxTime: gvDtNowFormatted()
 });
 
 //var Bday = dateFormat(new Date($('#fBeginDate').val()), 'yyyy/mm/dd HH:MM');
@@ -28,7 +44,9 @@ $('#fBeginDate').datetimepicker({
     dayOfWeekStart: 1,
     lang: 'en',
     value: $('#fBeginDate').val(),
-    step: 5
+    step: 5,
+    maxDate: gvDateOnlyFormatted(),
+    maxTime: gvDtNowFormatted()
 });
 
 $('#fEndDate').datetimepicker({
@@ -36,5 +54,7 @@ $('#fEndDate').datetimepicker({
     dayOfWeekStart: 1,
     lang: 'en',
     value: $('#fEndDate').val(),
-    step: 5
+    step: 5,
+    maxDate: gvDateOnlyFormatted(),
+    maxTime: gvDtNowFormatted()
 });
